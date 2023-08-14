b09f6b61fc7730769382ade1ff390ce74bb95f29
festus-sulumeti <sulumetifestus@outlook.com>
2023-08-14 09:12:54 +0000

    Update divisible_by_2 function to use nested if and if not statements

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,12 +1,9 @@
 #!/usr/bin/python3
-
 def divisible_by_2(my_list=[]):
     new_list = my_list.copy()
     for i in range(len(new_list)):
         if new_list[i] % 2 == 0:
-            if not new_list[i] == True:
-                new_list[i] = True
+            new_list[i] = True
         else:
-            if new_list[i] == True:
-                new_list[i] = False
+            new_list[i] = False
     return new_list
