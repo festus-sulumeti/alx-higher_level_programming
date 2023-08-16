c1fa908ade0a32f441705f7e05bf4dff5f82dbc8
festus-sulumeti <sulumetifestus@outlook.com>
2023-08-16 13:05:25 +0000

    Enhance simple_delete function to efficiently remove key from dictionary

--- 0x04-python-more_data_structures/8-simple_delete.py
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
-
 def simple_delete(a_dictionary, key=""):
-    if not key in a_dictionary:
+    if key in a_dictionary:
+        del a_dictionary[key]
         return a_dictionary
     else:
-        del a_dictionary[key]
         return a_dictionary
