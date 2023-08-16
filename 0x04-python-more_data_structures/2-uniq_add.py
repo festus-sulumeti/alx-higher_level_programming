75071f1b6ef4d5a557af73082520ea57fce2c5be
festus-sulumeti <sulumetifestus@outlook.com>
2023-08-16 11:23:37 +0000

    Fix uniq_add function to correctly calculate unique sum

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,9 +1,4 @@
 #!/usr/bin/python3
-
 def uniq_add(my_list=[]):
-    sum = 0
-    for num in my_list:
-        if num not in my_list:
-            my_list.append(num)
-            sum += num
-    return sum
+    x = list(set(my_list))
+    return (sum(x))
