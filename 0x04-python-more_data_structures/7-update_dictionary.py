8666acb911acca9b330ff11ae3a653fb6a7035a1
festus-sulumeti <sulumetifestus@outlook.com>
2023-08-16 12:47:53 +0000

    Improve update_dictionary function logic for better key-value assignment

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,9 +1,4 @@
 #!/usr/bin/python3
-
 def update_dictionary(a_dictionary, key, value):
-    if key in a_dictionary:
-        a_dictionary[key] = value
-    else:
-        a_dictionary[key] = []
-        a_dictionary[key].append(value)
+    a_dictionary[key] = value
     return a_dictionary
