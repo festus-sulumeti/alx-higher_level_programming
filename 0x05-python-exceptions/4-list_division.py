9e593919332431b37f3a60a6ddea01c58882abca
festus-sulumeti <sulumetifestus@outlook.com>
2023-08-28 11:25:36 +0000

    divides element by element 2 lists.

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
-
 def list_division(my_list_1, my_list_2, list_length):
-    new_lis = []
+    new_list = []
     for i in range(list_length):
         try:
             result = my_list_1[i]/my_list_2[i]
@@ -16,4 +15,4 @@ def list_division(my_list_1, my_list_2, list_length):
             result = 0
         finally:
             new_list.append(result)
-    return new_lis
+    return new_list
