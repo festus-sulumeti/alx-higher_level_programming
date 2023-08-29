6e070cefe7f9d83fe99f2e01ea983914cfe9f4e5
festus-sulumeti <sulumetifestus@outlook.com>
2023-08-29 12:19:41 +0000

    class Square that defines a square by: (based on 1-square.py)

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,15 +1,14 @@
-#!/usr/bin/python3
-# 2-square.py
-# festus sulumeti
-
-"""Size validation: """
-
-class Square(object):
-    """class variable size"""
-    def __init__(self, size=0):
-        """initialize size"""
-        if type(size) != int:
-            raise TypeError("size must be an integer")
-        elif size < 0:
-            raise ValueError("size must be >= 0")
-        self.__size = size
+def matrix_divided(matrix, div):
+    """ divides matrix by input divisor"""
+    if div == 0:
+        raise ZeroDivisionError('division by zero')
+    if not isinstance(div, (int, float)):
+        raise TypeError('div must be a number')
+    new = []
+    for row in matrix:
+        # This line creates a new list to store the elements of the current row,
+        # after dividing them by the divisor.
+        sub = [round((i / div), 2) for i in row]
+        new.append(sub)
+    # This line returns the new list of rows.
+    return new
