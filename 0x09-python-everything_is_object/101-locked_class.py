c2d37b19df3bc332c71f071ab2748f6f6e32d114
festus-sulumeti <sulumetifestus@outlook.com>
2023-09-05 12:51:23 +0000

    class LockedClass with no class or object attribute,

---
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+# 101-locked_class.py
+# festus sulumeti
+""" File name : 101-locked_class.py
+    Low memory cost: Defines a locked class
+    It is not allowed to import any module
+"""
+
+
+class LockedClass:
+    """
+    Prevent the user from instantiating new LockedClass attributes
+    for anything but attributes called 'first_name'.
+    """
+
+    __slots__ = ["first_name"]
