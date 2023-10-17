e676bd0797cda26eaf67493f2e029abc82dafd03
festus-sulumeti <sulumetifestus@outlook.com>
2023-10-17 20:31:00 +0000

    Go to UTF8

---
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -0,0 +1,6 @@
+-- Switch to the 'hbtn_0c_0' database
+USE `hbtn_0c_0`;
+
+-- Alter the 'first_table' to use the UTF-8 character set
+ALTER TABLE `first_table`
+CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
